a45bd7fec6acba9b9d8e7476e8ad04ca09567221
Vinicius Prado <vinicavagna@gmail.com>
2020-07-21 11:37:33 -0300

    Primeiro commit. Funcionalidades: Musica e Kick

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const Discord = require('discord.js');
-const { prefix, token} = require('./config.json')
+const {token, prefix} = require('./config.json');
+var dotenv = require('dotenv');
+dotenv.config();
+
+
 const client = new Discord.Client();
 const ytdl  = require('ytdl-core');
+
+
 
 const queue = new Map();
 
