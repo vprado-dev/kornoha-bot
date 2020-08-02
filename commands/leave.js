5b14e947685a3625344c00d5e2960a7a27c8c5db
Vinicius Prado <vinicavagna@gmail.com>
2020-08-02 00:49:39 -0300

    Resolução de bugs. Dar !leave enquanto há uma música tocando

--- commands/leave.js
+++ commands/leave.js
@@ -1,15 +1,18 @@
+require('./join');
 module.exports = {
     name: "leave",
     description: "Faz com que o bot se desconecte do canal que você está",
     async execute(message){
         const voiceChannel =  message.member.voice.channel;
-
+        const queue = message.client.queue;
+        const guild = message.guild;
         if(!voiceChannel)
             return message.channel.send(
                 "Você precisa estar conectado a um canal de voz né amigão"
             );
         try {
-            var connection = await voiceChannel.leave();
+            connection = await voiceChannel.leave();
+            queue.delete(guild.id);
         } catch (err) {
             console.log(err);
             return message.channel.send(err);
